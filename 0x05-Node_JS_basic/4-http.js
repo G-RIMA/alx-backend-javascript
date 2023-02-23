551b3913ed79c196085869e354fd6fabc39a4504
G-RIMA <gikandimaria178@gmail.com>
2023-02-23 08:26:15 +0300

    Create a small HTTP server using Nodes HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const app = require('http');
+
+const server = app.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Holberton School!');
+});
+
+server.listen(1245, () => {
+  console.log('Server running at http://localhost:1245/');
+});
+
+module.exports = app;
