425f83577963ec63438d952fa06766373466f239
G-RIMA <gikandimaria178@gmail.com>
2023-02-23 09:00:47 +0300

    passed eslint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,17 +1,15 @@
 const http = require('http');
-const fs = require('fs');
-
 
 const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
 const DATABASE = args[0];
 
-const app = http.createServer( async(req, res) => {
+const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   const { url } = req;
-  
+
   if (url === '/') {
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
@@ -19,17 +17,17 @@ const app = http.createServer( async(req, res) => {
     try {
       const students = await countStudents(DATABASE);
       res.end(`${students.join('\n')}`);
-      } catch(error){
-        res.end(error.message);
-      }
-    };
-  
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+
   res.statusCode = 404;
   res.end('Not found');
 });
 
 app.listen(1245, () => {
-  console.log('Server listening on port 1245');
+  // console.log('Server listening on port 1245');
 });
 
 module.exports = app;
